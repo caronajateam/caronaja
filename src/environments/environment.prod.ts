3ae2c3d0bf6665f7c49aa363e7a557b323580d5b
Gabriel Esteves <gabrielarevaloe@gmail.com>
2021-07-21 19:23:16 -0300

    arrumando firebase e api google maps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyDB2cozYv9TfJEZP3vEH5cLC0EZ6ywfgVY",
-    authDomain: "caronaja-b1b35.firebaseapp.com",
-    projectId: "caronaja-b1b35",
-    storageBucket: "caronaja-b1b35.appspot.com",
-    messagingSenderId: "158506675756",
-    appId: "1:158506675756:web:f80f3da2290d6501242b89"
+    apiKey: "AIzaSyDNmD0t49-LLsm6PmzUVPaGsnCWAV3ITxs",
+    authDomain: "caronajaa.firebaseapp.com",
+    projectId: "caronajaa",
+    storageBucket: "caronajaa.appspot.com",
+    messagingSenderId: "423471972338",
+    appId: "1:423471972338:web:2c9cafe4750d4fd1c6b56b"
   }
 };
